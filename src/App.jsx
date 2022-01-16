831459176f67f22603a01c51c9c97aecb1d811ad
Derek <derekp098@gmail.com>
2022-01-15 20:39:50 -0800

    cleaned up some stuff lol

--- src/App.jsx
+++ src/App.jsx
@@ -36,37 +36,6 @@ function App() {
     setCurrentPeriod('firstPeriod');
   }
 
-  // function useKey(key) {
-  //   // Keep track of key state
-  //   const [pressed, setPressed] = useState(false)
-
-  //   // Does an event match the key we're watching?
-  //   const match = event => key.toLowerCase() === event.key.toLowerCase()
-
-  //   // Event handlers
-  //   const onDown = event => {
-  //     if (match(event)) setPressed(true)
-  //   }
-
-  //   const onUp = event => {
-  //     if (match(event)) setPressed(false)
-  //   }
-
-  //   // Bind and unbind events
-  //   useEffect(() => {
-  //     window.addEventListener("keydown", onDown)
-  //     window.addEventListener("keyup", onUp)
-  //     return () => {
-  //       window.removeEventListener("keydown", onDown)
-  //       window.removeEventListener("keyup", onUp)
-  //     }
-  //   }, [key])
-
-  //   return pressed
-  // }
-
-  // if(useKey('k')) console.log('hello');
-
   function handleKeyPresses(e) {
     var value = e.key;
 
